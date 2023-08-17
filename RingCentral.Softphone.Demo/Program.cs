bb891192c01ab5bfd977cb95ef64d86d3c36dc82
Tyler Long <tyler4long@gmail.com>
2023-08-17 10:53:26 -0700

    Try some WSS code

--- RingCentral.Softphone.Demo/Program.cs
+++ RingCentral.Softphone.Demo/Program.cs
@@ -10,7 +10,8 @@ using RingCentral.Softphone.Net;
 using SIPSorcery.Net;
 using SIPSorcery.SIP.App;
 using SIPSorceryMedia.Abstractions;
-using Nager.TcpClient;
+using Websocket.Client;
+using System.Net.WebSockets;
 
 namespace RingCentral.Softphone.Demo
 {
@@ -36,25 +37,35 @@ namespace RingCentral.Softphone.Demo
                     {
                         new SIPInfoRequest
                         {
-                            transport = "TCP"
+                            transport = "WSS"
                         }
-                    },
-                    device = new DeviceInfoRequest
-                    {
-                        computerName = Environment.MachineName
                     }
                 });
                 var sipInfo = sipProvision.sipInfo[0];
-
-                var client = new TcpClient();
-                var tokens = sipInfo.outboundProxy.Split(":");
-                await client.ConnectAsync(tokens[0], int.Parse(tokens[1]));
-                var rtpSession = new RTPSession(false, false, false);
-
+                var wsUri = "wss://" + sipInfo.outboundProxy;
+                var factory = new Func<ClientWebSocket>(() =>
+                {
+                    var cws = new ClientWebSocket();
+                    cws.Options.AddSubProtocol("sip");
+                    return cws;
+                });
+                var ws = new WebsocketClient(new Uri(wsUri), factory);
+                ws.ReconnectTimeout = null;
+                ws.MessageReceived.Subscribe(responseMessage =>
+                {
+                   Console.WriteLine(responseMessage.Text);
+                });
+                ws.DisconnectionHappened.Subscribe(info =>
+                {
+                    Console.WriteLine(info.CloseStatusDescription);
+                });
+                await ws.Start();
+                
+                
                 var userAgent = "RingCentral.Softphone.Net";
                 var fakeDomain = $"{Guid.NewGuid().ToString()}.invalid";
                 var fakeEmail = $"{Guid.NewGuid().ToString()}@{fakeDomain}";
-
+                
                 var registrationMessage = new SipMessage($"REGISTER sip:{sipInfo.domain} SIP/2.0",
                     new Dictionary<string, string>
                     {
@@ -68,123 +79,132 @@ namespace RingCentral.Softphone.Demo
                         {"Content-Length", "0"},
                         {"Max-Forwards", "70"}
                     }, "");
-
-                var cachedMessages = "";
-
-                // receive message
-                async void OnDataReceived(byte[] receivedData)
-                {
-                    var data = Encoding.UTF8.GetString(receivedData);
-                    Console.WriteLine("Receiving...\n" + data);
-                    cachedMessages += data;
-                }
-
-                client.DataReceived += OnDataReceived;
-
-                // send message
-                async void SendMessage(SipMessage sipMessage)
-                {
-                    var message = sipMessage.ToMessage();
-                    Console.WriteLine("Sending...\n" + message);
-                    var bytes = Encoding.UTF8.GetBytes(message);
-                    await client.SendAsync(bytes);
-                }
-
-                // send first registration message
-                SendMessage(registrationMessage);
-
-                // wait for server messages forever
-                while (true) 
-                {
-                    await Task.Delay(100);
-                    if (cachedMessages.Length > 0)
-                    {
-                        // just in case, sometimes we only receive half a message, wait for the other half
-                        await Task.Delay(100); 
-                        
-                        var tempMessages = cachedMessages.Split("\r\n\r\nSIP/2.0 ");
-                        // sometimes we receive two messages in one data
-                        if (tempMessages.Length > 1)
-                        {
-                            // in this case, we only need the second one
-                            cachedMessages = "SIP/2.0 " + tempMessages[1];
-                        }
-
-                        var sipMessage = SipMessage.FromMessage(cachedMessages);
-
-                        // reset variables
-                        cachedMessages = "";
-
-                        // the message after we reply to INVITE
-                        if (sipMessage.Subject.StartsWith("ACK sip:"))
-                        {
-                            // The purpose of sending a DTMF tone is if our SDP had a private IP address then the server needs to get at least
-                            // one RTP packet to know where to send.
-                            await rtpSession.SendDtmf(0, CancellationToken.None);
-                        }
-
-                        // authorize failed with nonce in header
-                        if (sipMessage.Subject.StartsWith("SIP/2.0 401 Unauthorized"))
-                        {
-                            var nonceMessage = sipMessage;
-                            var wwwAuth = "";
-                            if (nonceMessage.Headers.ContainsKey("WWW-Authenticate"))
-                            {
-                                wwwAuth = nonceMessage.Headers["WWW-Authenticate"];
-                            }
-                            else if (nonceMessage.Headers.ContainsKey("Www-Authenticate"))
-                            {
-                                wwwAuth = nonceMessage.Headers["Www-Authenticate"];
-                            }
-
-                            var regex = new Regex(", nonce=\"(.+?)\"");
-                            var match = regex.Match(wwwAuth);
-                            var nonce = match.Groups[1].Value;
-                            var auth = Net.Utils.GenerateAuthorization(sipInfo, "REGISTER", nonce);
-                            registrationMessage.Headers["Authorization"] = auth;
-                            registrationMessage.Headers["CSeq"] = "8083 REGISTER";
-                            registrationMessage.Headers["Via"] =
-                                $"SIP/2.0/TCP {fakeDomain};branch=z9hG4bK{Guid.NewGuid().ToString()}";
-                            SendMessage(registrationMessage);
-                        }
-
-                        // whenever there is an inbound call
-                        if (sipMessage.Subject.StartsWith("INVITE sip:"))
-                        {
-                            var inviteSipMessage = sipMessage;
-                            MediaStreamTrack audioTrack = new MediaStreamTrack(new List<AudioFormat>
-                                {new AudioFormat(SDPWellKnownMediaFormatsEnum.PCMU)});
-                            rtpSession.addTrack(audioTrack);
-                            var result =
-                                rtpSession.SetRemoteDescription(SdpType.offer,
-                                    SDP.ParseSDPDescription(inviteSipMessage.Body));
-                            Console.WriteLine(result);
-                            var answer = rtpSession.CreateAnswer(null);
-
-                            rtpSession.OnRtpPacketReceived +=
-                                (IPEndPoint remoteEndPoint, SDPMediaTypesEnum mediaType, RTPPacket rtpPacket) =>
-                                {
-                                    Console.WriteLine("OnRtpPacketReceived");
-                                };
-
-                            sipMessage =
-                                new SipMessage("SIP/2.0 200 OK", new Dictionary<string, string>
-                                {
-                                    {"Contact", $"<sip:{fakeEmail};transport=tcp>"},
-                                    {"Content-Type", "application/sdp"},
-                                    {"Content-Length", answer.ToString().Length.ToString()},
-                                    {"User-Agent", "RingCentral.Softphone.Net"},
-                                    {"Via", inviteSipMessage.Headers["Via"]},
-                                    {"From", inviteSipMessage.Headers["From"]},
-                                    {"To", $"{inviteSipMessage.Headers["To"]};tag={Guid.NewGuid().ToString()}"},
-                                    {"CSeq", inviteSipMessage.Headers["CSeq"]},
-                                    {"Supported", "outbound"},
-                                    {"Call-Id", inviteSipMessage.Headers["Call-Id"]}
-                                }, answer.ToString());
-                            SendMessage(sipMessage);
-                        }
-                    }
-                }
+                ws.Send(registrationMessage.ToMessage());
+                
+                await Task.Delay(999999999);
+                // var client = new TcpClient();
+                // var tokens = sipInfo.outboundProxy.Split(":");
+                // await client.ConnectAsync(tokens[0], int.Parse(tokens[1]));
+                // var rtpSession = new RTPSession(false, false, false);
+                //
+       
+                //
+                // var cachedMessages = "";
+                //
+                // // receive message
+                // async void OnDataReceived(byte[] receivedData)
+                // {
+                //     var data = Encoding.UTF8.GetString(receivedData);
+                //     Console.WriteLine("Receiving...\n" + data);
+                //     cachedMessages += data;
+                // }
+                //
+                // client.DataReceived += OnDataReceived;
+                //
+                // // send message
+                // async void SendMessage(SipMessage sipMessage)
+                // {
+                //     var message = sipMessage.ToMessage();
+                //     Console.WriteLine("Sending...\n" + message);
+                //     var bytes = Encoding.UTF8.GetBytes(message);
+                //     await client.SendAsync(bytes);
+                // }
+                //
+                // // send first registration message
+                // SendMessage(registrationMessage);
+                //
+                // // wait for server messages forever
+                // while (true) 
+                // {
+                //     await Task.Delay(100);
+                //     if (cachedMessages.Length > 0)
+                //     {
+                //         // just in case, sometimes we only receive half a message, wait for the other half
+                //         await Task.Delay(100); 
+                //         
+                //         var tempMessages = cachedMessages.Split("\r\n\r\nSIP/2.0 ");
+                //         // sometimes we receive two messages in one data
+                //         if (tempMessages.Length > 1)
+                //         {
+                //             // in this case, we only need the second one
+                //             cachedMessages = "SIP/2.0 " + tempMessages[1];
+                //         }
+                //
+                //         var sipMessage = SipMessage.FromMessage(cachedMessages);
+                //
+                //         // reset variables
+                //         cachedMessages = "";
+                //
+                //         // the message after we reply to INVITE
+                //         if (sipMessage.Subject.StartsWith("ACK sip:"))
+                //         {
+                //             // The purpose of sending a DTMF tone is if our SDP had a private IP address then the server needs to get at least
+                //             // one RTP packet to know where to send.
+                //             await rtpSession.SendDtmf(0, CancellationToken.None);
+                //         }
+                //
+                //         // authorize failed with nonce in header
+                //         if (sipMessage.Subject.StartsWith("SIP/2.0 401 Unauthorized"))
+                //         {
+                //             var nonceMessage = sipMessage;
+                //             var wwwAuth = "";
+                //             if (nonceMessage.Headers.ContainsKey("WWW-Authenticate"))
+                //             {
+                //                 wwwAuth = nonceMessage.Headers["WWW-Authenticate"];
+                //             }
+                //             else if (nonceMessage.Headers.ContainsKey("Www-Authenticate"))
+                //             {
+                //                 wwwAuth = nonceMessage.Headers["Www-Authenticate"];
+                //             }
+                //
+                //             var regex = new Regex(", nonce=\"(.+?)\"");
+                //             var match = regex.Match(wwwAuth);
+                //             var nonce = match.Groups[1].Value;
+                //             var auth = Net.Utils.GenerateAuthorization(sipInfo, "REGISTER", nonce);
+                //             registrationMessage.Headers["Authorization"] = auth;
+                //             registrationMessage.Headers["CSeq"] = "8083 REGISTER";
+                //             registrationMessage.Headers["Via"] =
+                //                 $"SIP/2.0/TCP {fakeDomain};branch=z9hG4bK{Guid.NewGuid().ToString()}";
+                //             SendMessage(registrationMessage);
+                //         }
+                //
+                //         // whenever there is an inbound call
+                //         if (sipMessage.Subject.StartsWith("INVITE sip:"))
+                //         {
+                //             var inviteSipMessage = sipMessage;
+                //             MediaStreamTrack audioTrack = new MediaStreamTrack(new List<AudioFormat>
+                //                 {new AudioFormat(SDPWellKnownMediaFormatsEnum.PCMU)});
+                //             rtpSession.addTrack(audioTrack);
+                //             var result =
+                //                 rtpSession.SetRemoteDescription(SdpType.offer,
+                //                     SDP.ParseSDPDescription(inviteSipMessage.Body));
+                //             Console.WriteLine(result);
+                //             var answer = rtpSession.CreateAnswer(null);
+                //
+                //             rtpSession.OnRtpPacketReceived +=
+                //                 (IPEndPoint remoteEndPoint, SDPMediaTypesEnum mediaType, RTPPacket rtpPacket) =>
+                //                 {
+                //                     Console.WriteLine("OnRtpPacketReceived");
+                //                 };
+                //
+                //             sipMessage =
+                //                 new SipMessage("SIP/2.0 200 OK", new Dictionary<string, string>
+                //                 {
+                //                     {"Contact", $"<sip:{fakeEmail};transport=tcp>"},
+                //                     {"Content-Type", "application/sdp"},
+                //                     {"Content-Length", answer.ToString().Length.ToString()},
+                //                     {"User-Agent", "RingCentral.Softphone.Net"},
+                //                     {"Via", inviteSipMessage.Headers["Via"]},
+                //                     {"From", inviteSipMessage.Headers["From"]},
+                //                     {"To", $"{inviteSipMessage.Headers["To"]};tag={Guid.NewGuid().ToString()}"},
+                //                     {"CSeq", inviteSipMessage.Headers["CSeq"]},
+                //                     {"Supported", "outbound"},
+                //                     {"Call-Id", inviteSipMessage.Headers["Call-Id"]}
+                //                 }, answer.ToString());
+                //             SendMessage(sipMessage);
+                //         }
+                //     }
+                // }
             }).GetAwaiter().GetResult();
         }
     }
